c8e5b1701a195b4c862c8a0eb497f4f68c1ccd67
alicyapocket <alicyaferreira36@gmail.com>
2026-03-09 16:36:15 -0300

    Add files via upload

---
+++ teste_idade.py
@@ -0,0 +1,12 @@
+print('digite a idade do jovem')
+
+idade = int(input())
+
+if idade <= 18:
+    print('jovem é menor de idade')
+
+elif idade <= 25:
+    print('Adulto')
+
+else:
+    print('jovem é idoso de idade')
